51d03cf50f04dd77215072cd34ae891ec2b818ef
Louise <louiseneves87@gmail.com>
2026-06-04 13:11:47 -0300

    Versão 27

--- app.config.js
+++ app.config.js
@@ -28,7 +28,7 @@ export default {
 
     android: {
       package: "com.louiseneves.calculardizimo",
-      versionCode: 26,
+      versionCode: 27,
 
       adaptiveIcon: {
         foregroundImage: "./assets/logo.png",
