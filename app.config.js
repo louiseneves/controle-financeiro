3ba0aba10601e92060b89b9c4521b3fc8b6ea7f1
Louise <louiseneves87@gmail.com>
2026-06-08 12:10:58 -0300

    Implementação do Google Play Billing

--- app.config.js
+++ app.config.js
@@ -28,7 +28,7 @@ export default {
 
     android: {
       package: "com.louiseneves.calculardizimo",
-      versionCode: 27,
+      versionCode: 28,
 
       adaptiveIcon: {
         foregroundImage: "./assets/logo.png",
